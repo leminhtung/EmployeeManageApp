a7772a15d0a8776c6ba22d65f6516d79bdd4f5c0
Minh Tùng <leminhtung1111998@gmail.com>
2019-08-02 18:31:36 +0700

    new

---
+++ EmployeeManage.Unittest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EmployeeManage.Unittest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
